6b18d2062c1003fd463b81c5080fc2c641032545
Binh19032004 <thanhbinh19032004@gmail.com>
2025-04-20 15:08:06 +0700

    Initial commit

---
+++ src/components/UserPhotos/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Typography, Divider } from "@mui/material";
+import { Link, useParams } from "react-router-dom";
+import models from "../../modelData/models";
+
+function formatDate(dateStr) {
+  return new Date(dateStr).toLocaleString();
+}
+
+function UserPhotos() {
+  const { userId } = useParams();
+  const photos = models.photoOfUserModel(userId);
+  const user = userId ? models.userModel(userId) : null;
+  const userName = user ? `${user.first_name} ${user.last_name}` : null;
+  return (
+    <div>
+      <Typography variant="h5" gutterBottom>
+        Photos of User {userName}
+      </Typography>
+      {photos.map((photo) => (
+        <div key={photo._id} style={{ marginBottom: "20px" }}>
+          {/* Sử dụng ảnh từ thư mục public/images */}
+          <img
+            src={`/images/${photo.file_name}`}
+            alt=""
+            style={{ maxWidth: "100%", maxHeight: "400px" }}
+          />
+          <Typography variant="caption">
+            Created: {formatDate(photo.date_time)}
+          </Typography>
+          <div style={{ marginTop: "10px" }}>
+            {photo.comments?.map((comment) => (
+              <div key={comment._id}>
+                <Typography variant="body2">
+                  <strong>
+                    <Link to={`/users/${comment.user._id}`}>
+                      {comment.user.first_name} {comment.user.last_name}
+                    </Link>
+                  </strong>{" "}
+                  at {formatDate(comment.date_time)}:
+                </Typography>
+                <Typography variant="body1" style={{ marginLeft: "10px" }}>
+                  {comment.comment}
+                </Typography>
+              </div>
+            ))}
+          </div>
+          <Divider style={{ marginTop: "15px" }} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default UserPhotos;
